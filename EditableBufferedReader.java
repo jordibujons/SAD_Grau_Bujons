7255620722246c21323d2646cd1405895890a3f2
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-09-22 13:21:14 +0200

    editableBufferedReader

---
+++ EditableBufferedReader.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class EditableBufferedReader extends BufferedReader{
+
+    public EditableBufferedReader(Reader in) {
+        super(in);
+    }
+
+    public static void setRaw (){
+       
+    }
+    public static void unSetRaw (){
+        
+    }
+    public int read(){
+        return 0;
+        
+    }
+    public String readLine() {
+        return null;
+        
+    } 
+    
+}
+
