9ea0af3f04d41d88e2f893ddccafb3829cd4eae5
MarcGrauPujol <75749590+MarcGrauPujol@users.noreply.github.com>
2021-09-29 12:30:11 +0200

    Add files via upload

---
+++ teoria/binari_classe.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int x, i, j, bin[100];
+
+    printf("hola pola gorda");
+
+        while(scanf("%d", &x) != EOF){  //EOF és equivalent a -1
+            i=0;
+            while (x>0)
+            {
+                bin[i] = x % 2;
+                x = x/2;
+                i++;
+            }          
+
+        }
+        for(j=i; j>=0; j--){
+            printf("%d", bin[j]);
+        }
+}
+
+//funcio decabin(x, &resto_div)
---
+++ teoria/columnes.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//  minBufSize (class MinBufSize)
+//                Specify the minimum size of the input buffer, i.e., the amount
+//                of data that xterm requests on each read.  The default is
+//                "4096".  You cannot set this to a value less than 64.
+
