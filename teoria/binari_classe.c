6c6ada5f0f865351b023f8e9e27618a4b9721525
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-09-29 16:00:34 +0200

    binari definitiu corregit

--- teoria/binari_classe.c
+++ teoria/binari_classe.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
     int x, i, j, bin[100];
@@ -6,18 +7,8 @@ int main(void){
     printf("exercici numeros binaris, escriu el numero que vols convertir de decimal a binari:");
 
         while(scanf("%d", &x) != EOF){  //EOF és equivalent a -1
-            i=0;
-            while (x>0)
-            {
-                bin[i] = x % 2;
-                x = x/2;
-                i++;
-            }          
-
-        }
-        for(j=i; j>=0; j--){
-            printf("%d", bin[j]);
-        }
-}
-
-//funcio decabin(x, &resto_div)
+            fwrite( &i, sizeof(i), 1, stdout);  //size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+                                                //ptr − This is the pointer to the array of elements to be written.
+        }                                       //size − This is the size in bytes of each element to be written.
+        return EXIT_SUCCESS;                     //nmemb − This is the number of elements, each one with a size of size bytes.
+}                                           //stream − This is the pointer to a FILE object that specifies an output stream.
