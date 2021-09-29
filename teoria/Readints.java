a8b583f81042c4bddce20a2f070074114df6b41c
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-09-29 17:01:48 +0200

    readInts java

--- teoria/Readints.java
+++ teoria/Readints.java
@@ -1,6 +1,20 @@
-public class ReadInts{
-    public static void main(String[] args){
-        
+import java.io.*;
+import java.io.DataInputStream;
+import java.io.EOFException;
 
+public class ReadInts {
+    public static void main(String[] args){
+
+        DataInputStream in = new DataInputStream(System.in);
+        int i = 0;
+        
+        while(true){
+            try{
+                i = in.readInt();
+            }catch(EOFException e){
+                break;
+            }
+        }
+        System.out.println(i); 
     }
 }
