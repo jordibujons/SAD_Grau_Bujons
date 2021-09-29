539ae42bde5cbb9ac0eb99783db748627caea9c2
jordibujons <jordibujons11@gmail.com>
2021-09-29 13:02:13 +0200

    binari acabat

--- teoria/binari_classe.c
+++ teoria/binari_classe.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
     int x, i, j, bin[100];
 
-    printf("exercici numeros binaris");
+    printf("exercici numeros binaris, escriu el numero que vols convertir de decimal a binari:");
 
         while(scanf("%d", &x) != EOF){  //EOF és equivalent a -1
             i=0;
