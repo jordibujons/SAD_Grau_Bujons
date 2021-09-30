4cdf647a372db88076bf8464938aeb4815261d9e
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-09-30 19:44:00 +0200

    ultima mod

--- practica1/Line.java
+++ practica1/Line.java
@@ -1,11 +1,20 @@
 package practica1;
 
 import java.io.*;
+import java.util.*;
 
 public class Line {
 
+    ArrayList<Integer> line;
     private int cursor, length;
     private boolean insert;
+
+    public Line(){
+        this.cursor = 0;
+        this.length = 0;
+        this.insert = false;
+        this.line = new ArrayList<>();
+    }
 
     public int getPos(){
         return this.cursor;
@@ -24,6 +33,16 @@ public class Line {
     }
 
     public void suprCar(){          //suprimir car de la dreta
+
+    }
+
+    public void startLine(){
+        this.cursor = 0;
+
+    }
+
+    public void endLine(){
+        this.cursor = this.line.size();
 
     }
 
