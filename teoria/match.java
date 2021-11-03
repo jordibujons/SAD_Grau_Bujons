cc06c4bc31a2eca440c64d438f8b289094413ff9
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-11-03 16:58:33 +0100

    classe

--- teoria/match.java
+++ teoria/match.java
@@ -1,14 +1,20 @@
+import java.io.IOException;
 
-private boolean match(String str) throws IOEx{
-    boolean b=true;
-    mark(str.length());     //podem llegir fins la longitud de str i tirar enrere si convé
 
-    for(int i=0; i<str.length(); i++){
-        b = str.charAt(i) == super.read();
-        if(!b || ! ready() && i < str.length()-1){  //ready comproba que tinguid contigut en els dos strings per no seguir comparant quan un dels dos ja s'ha acabat 
-            reset();        //tornar a llegir des del principi
-            break;
+public class match {
+
+    private boolean match(String str) throws IOException {
+        boolean b = true;
+        mark(str.length()); // podem llegir fins la longitud de str i tirar enrere si convé
+
+        for (int i = 0; i < str.length(); i++) {
+            b = str.charAt(i) == super.read();
+            if (!b || !ready() && i < str.length() - 1) { // ready comproba que tinguid contigut en els dos strings per
+                                                          // no seguir comparant quan un dels dos ja s'ha acabat
+                reset(); // tornar a llegir des del principi
+                break;
+            }
         }
+        return b;
     }
-    return b;
 }
---
+++ teoria/recursiveDescentParsing.java
@@ -0,0 +1,13 @@
+package teoria;
+
+import org.apache.commons.lang3.mutable;
+
+public class recursiveDescentParsing{
+
+    boolean parseLine(){
+
+    }
+
+    boolean parseExression()
+
+}
