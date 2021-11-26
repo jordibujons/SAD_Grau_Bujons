38e1247c9323fab35861a4613bfba9a4ce01e2fe
MarcGrauPujol <marc.grau.i@estudiantat.upc.edu>
2021-11-26 15:15:14 +0100

    snake

--- snake project/js/snake.js
+++ snake project/js/snake.js
@@ -6,18 +6,18 @@ const SNAKE_BODY = [
 ]     //posició (10,10) és el centre del taulell
 
 export function update(){
-    for (let i = SNAKE_BODY.length; i <= 0; i--) {
-        SNAKE_BODY[i+1] = SNAKE_BODY[i]
+    for (let i = SNAKE_BODY.length-2; i >= 0; i--) {    //l'última posició de la serp ens és igual pq desaperaixerà. Com que el nostre vector (serp) comença a 0, si fem (lenght -2) estarem apuntant a la penúltima pos de la serp 
+        SNAKE_BODY[i+1] = {...SNAKE_BODY[i]}    //{... } fer nou objecte sense que agafi referències del vell
     }
 
-    SNAKE_BODY[0].x += 1
-    SNAKE_BODY[0].y += 0
+    // SNAKE_BODY[0].x += 0
+    // SNAKE_BODY[0].y += 1
 }
 export function draw(GameBoard){
-    SNAKE_BODY.forEach(segment => {
+    SNAKE_BODY.forEach(seg => {
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.style.gridRowStart = seg.y
+        snakeElement.style.gridColumnStart = seg.x
         snakeElement.classList.add('snakeBody')
         GameBoard.appendChild(snakeElement)
         })
